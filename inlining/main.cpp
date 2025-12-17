d8bc852cfc67e9ba660143ee88b72f233a3ba26b
Anton Lipkin <anton.lipkin@myoffice.team>
2025-12-17 23:12:16 +0500

    inlining benchmark

--- inlining/main.cpp
+++ inlining/main.cpp
@@ -1,31 +1,71 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <random>
 #include <benchmark/benchmark.h>
 
-// #if defined(__GNUC__) || defined(__clang__)
-//     #define FORCE_INLINE __attribute__((always_inline)) inline
-// #endif
+#if defined(__GNUC__) || defined(__clang__)
+    #define FORCE_INLINE __attribute__((always_inline)) inline
+    #define NOINLINE __attribute__((noinline))
+#endif
 
-void some_func() {
-    std::cout << "some_func" << std::endl;
+static char random_data[1024*1024];
+
+void initialize_random_data() {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(0, 255);
+    
+    for (size_t i = 0; i < sizeof(random_data); ++i) {
+        random_data[i] = static_cast<char>(dis(gen));
+    }
 }
 
-// Benchmark for some_func()
-static void BM_some_func(benchmark::State& state) {
-    // Redirect cout to suppress output during benchmarking
-    std::cout.setstate(std::ios_base::failbit);
+FORCE_INLINE void swap_chars_inlined(char& a, char& b) {
+    std::swap(a, b);
+}
+
+void process_random_data_inlined() {
+    size_t size = sizeof(random_data);
+    for (size_t i = 0; i < size / 2; ++i) {
+        swap_chars_inlined(random_data[i], random_data[size - 1 - i]);
+    }
+}
+
+NOINLINE void swap_chars_noinline(char& a, char& b) {
+    std::swap(a, b);
+}
+
+void process_random_data_noinline() {
+    size_t size = sizeof(random_data);
+    for (size_t i = 0; i < size / 2; ++i) {
+        swap_chars_noinline(random_data[i], random_data[size - 1 - i]);
+    }
+}
+
+// Benchmark для версии с FORCE_INLINE
+static void BM_process_inlined(benchmark::State& state) {
+    initialize_random_data();
     
     for (auto _ : state) {
-        some_func();
+        process_random_data_inlined();
+        benchmark::DoNotOptimize(random_data);
     }
-    
-    // Re-enable cout
-    std::cout.clear();
 }
 
-// Register the benchmark
-BENCHMARK(BM_some_func);
+// Benchmark для версии с NOINLINE
+static void BM_process_noinline(benchmark::State& state) {
+    initialize_random_data();
+    
+    for (auto _ : state) {
+        process_random_data_noinline();
+        benchmark::DoNotOptimize(random_data);
+    }
+}
+
+// Register the benchmarks
+BENCHMARK(BM_process_inlined);
+BENCHMARK(BM_process_noinline);
 
 // Main function to run benchmarks
 BENCHMARK_MAIN();
